414570770621f1a925a358213c0f4c305f9ed191
Soumya7777-cyber <Khandaliasoumyajitjana@gmail.com>
2024-08-03 20:50:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-A=int(input("A = "))
-B=input("Enter the operator: ")
-C=int(input("B = "))
-print(f"The answer of {A} {B} {C} = ",end="")
-if(B=="+"):
-    print(A+C)
-elif(B=="-"):
-    print(A-C)
-elif(B=="/"):
-    print(A/C)
-elif(B=="*"):
-    print(A*C)
+'''
+0 is for Rock
+1 is for Paper
+2 is for Scissors 
+ '''
+import random
+computer=random.choice([0,1,2])
+younum=input("Enter your option:")
+youdict={"rock":0,"paper":1,"scissors":2}
+you=youdict[younum]# your inputed value(like 0,1,2) store in to the younum
+revdict={0:"rock",1:"paper",2:"scissors"}
+print(f"you choose {younum}")
+print(f"computer choose {revdict[computer]}")
+if(computer==you):
+    print("it is draw")
+elif(computer==0 and you==1):
+        print("you win !")
+elif(computer==0 and you==2):
+        print("computer win !")
+elif(computer==1 and you==0):
+        print("computer win !")
+elif(computer==1 and you==2):
+        print("you win !")
+elif(computer==2 and you==0):
+        print("you win !")
+elif(computer==2 and you==1):
+        print("computer win !")
 else:
-    print("your input operator is wrong!")
+        print("Somthing Error !")
